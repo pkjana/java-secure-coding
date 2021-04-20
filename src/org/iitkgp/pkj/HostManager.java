65e2608a5f54ed2c30639a0cdb9b795d9d73e051
Jana, Pabitra (pj5867) <pj5867@att.com>
2021-04-20 16:49:26 +0530

    Get hostname securely

---
+++ src/org/iitkgp/pkj/HostManager.java
@@ -0,0 +1,37 @@
+package org.iitkgp.pkj;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+public class HostManager {
+
+	Logger logger = Logger.getLogger(HostManager.class.getName());
+
+	public static void main(String[] args) {
+		String hostName = getHostName().toUpperCase();
+		System.out.println("Hostname : " + hostName);
+	}
+
+	public static String getHostName() {
+		String os = System.getProperty("os.name").toLowerCase();
+		String hostName = null;
+		try {
+			if (os.contains("win")) {
+				hostName = System.getenv("COMPUTERNAME").trim();
+			} else if (os.contains("nix") || os.contains("nux") || os.contains("mac os x")) {
+				hostName = execReadToString("hostname").trim();
+			}
+		} catch (Exception e) {
+			Logger.getLogger(HostManager.class.getName()).info("Hostname not found:" + e.getMessage());
+		}
+		return hostName;
+	}
+
+	public static String execReadToString(String execCommand) throws IOException {
+		try (Scanner s = new Scanner(Runtime.getRuntime().exec(execCommand).getInputStream()).useDelimiter("\\A")) {
+			return s.hasNext() ? s.next() : "";
+		}
+	}
+
+}
